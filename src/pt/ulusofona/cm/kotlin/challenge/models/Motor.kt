7fef94219f5736ad6e102360fdcdcc0ab2fb98ba
Ruben Guerra <a21901521@alunos.ulht.pt>
2021-02-18 22:45:08 +0000

    Update error to dp test "pt/ulusofona/cm/kotlin/challenge/models/Motor.kt: (7, 9) 'ligado' hides member of supertype 'Ligavel' and needs 'override' modifier"

--- src/pt/ulusofona/cm/kotlin/challenge/models/Motor.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Motor.kt
@@ -4,7 +4,7 @@ import pt.ulusofona.cm.kotlin.challenge.interfaces.Ligavel
 
 data class Motor(val cavalos:Int, private val cilindrada:Int): Ligavel{
 
-    var ligado:Boolean = estaLigado()
+    override var ligado:Boolean = estaLigado()
 
     override fun toString(): String {
         return "Motor | $cavalos | $cilindrada"
